515733271fc0e22bea65c56d4ecede584b720912
Tim Hyson <tjhyson@gmail.com>
2015-10-25 10:15:45 +0000

    fixed spacing and minor syntax

--- app/js/app.js
+++ app/js/app.js
@@ -1,28 +1,29 @@
-(function(){
+(function() {
   'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('readingList', [])
+  angular.module('readingList', [])
 
-.controller('ReadingListController', function(){
+.controller('ReadingListController', function() {
   this.books = books;
   this.genres = genres;
 })
 
-.directive('bookGenres', function(){
+.directive('bookGenres', function() {
   return {
     restrict: 'E',
     templateUrl: 'partials/book-genres.html',
-    replace: true
-  }
+    scope: {
+      genres: '=',
+    },
+  };
 })
 
-.directive('bookCover', function(){
+.directive('bookCover', function() {
   return {
     restrict: 'E',
     templateUrl: 'partials/book-cover.html',
-    replace: true
-  }
+    replace: true,
+  };
 })
 
 .directive('reviewForm', function() {
@@ -30,49 +31,56 @@ angular.module('readingList', [])
     restrict: 'E',
     templateUrl: 'partials/review-form.html',
     replace: true,
-    controller: function(){
+    controller: function() {
       this.showForm = false;
-      this.book = {};
+      this.book = {genres:{}};
+
+      this.addReview = function(form) {
+        books.push(this.book);
+        this.book = {genres:{}};
+        form.$setPristine();
+      };
     },
+
     controllerAs: 'reviewFormCtrl',
     scope: {
-      books: "=",
-      genres: "="
-    }
-  }
+      books: '=',
+      genres: '=',
+    },
+  };
 });
 
-var genres = [ 'fable', 'fantasy', 'fiction', 'folklore', 'horror', 'humor', 'legend', 'metafiction', 'mystery', 'mythology', 'non-fiction', 'poetry' ];
+  var genres = ['fable', 'fantasy', 'fiction', 'folklore', 'horror', 'humor', 'legend', 'metafiction', 'mystery', 'mythology', 'non-fiction', 'poetry'];
 
-var books = [
+  var books = [
   {
     title: 'A Game of Thrones: A Song of Ice and Fire',
     author: 'George R.R. Martin',
     isbn: '0553593714',
     review: 'The most inventive and entertaining fantasy saga of our time—warrants one hell of an introduction. I loved this book!',
     rating: 4,
-    genres: { 'non-fiction': true, fantasy: true }
-  },{
+    genres: { 'non-fiction': true, fantasy: true },
+  }, {
     title: 'HTML for Babies',
     author: 'John C Vanden-Heuvel Sr',
     isbn: '0615487661',
     review: "It's never too early to be standards compliant! I taught my little one mark-up in under one hour!",
     rating: 5,
-    genres: { fiction: true }
-  },{
+    genres: { fiction: true },
+  }, {
     title: 'A is for Array',
     author: 'Brandon J Hansen',
     isbn: '1489522212',
     review: 'A is for Array is the ABC book for future programmers. Filled with fun illustrations and simple real-world examples, my children loved seeing my world intertwined with theirs!',
     rating: 4,
-    genres: { fiction: true }
-  },{
+    genres: { fiction: true },
+  }, {
     title: 'The Dragon Reborn',
     author: 'Robert Jordan',
     isbn: '0812513711',
     review: 'The Wheel weaves as the Wheel wills, and we are only the thread of the Pattern. Moiraine',
     rating: 4,
-    genres: { 'non-fiction': true, fantasy: true }
-  }
+    genres: { 'non-fiction': true, fantasy: true },
+  },
   ];
 })();
